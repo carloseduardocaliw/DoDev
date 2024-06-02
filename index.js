9908d83b6efe299e7fb2d38142dfa39d9257a3b1
Carlos <carlossccali@outlook.com>
2024-06-02 18:04:44 -0300

    novo teste

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ numero1 = parseInt(prompt('Insira um valor Inteiro e Positivo: '))
 
 for(let tabuada = 0; tabuada <= 10; tabuada++){
     console.log(numero1 + " X " + tabuada + " = " + numero1 * tabuada)
+    console.log()
 }
 
-
+for(let tabuada = 0; tabuada <= 10; tabuada++){
+    console.log()
+}
